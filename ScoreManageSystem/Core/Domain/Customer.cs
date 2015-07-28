a0088dc9897b10829b86caa3b2355e3738164e29
ta_wuhen <781739081@qq.com>
2015-07-28 23:24:26 +0800

    add model

---
+++ ScoreManageSystem/Core/Domain/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ScoreManageSystem.Domain;
+
+namespace ScoreManageSystem.Core.Domain
+{
+    public class Customer:BaseEntity
+    {
+        [DisplayName("姓名")]
+        [MaxLength(4, ErrorMessage = "姓名不能超过4个字")]
+        public string Name { get; set; }
+
+        [MaxLength(12, ErrorMessage = "不能超过12个字符")]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public DateTime? LastLoginTime { get; set; }
+
+    }
+}
